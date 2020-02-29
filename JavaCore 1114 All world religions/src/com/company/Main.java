6fb80413696c9bd0ac6d23eae79ed1d1088dfdf1
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-29 11:04:28 +0200

    Add files via upload

---
+++ JavaCore 1114 All world religions/src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+/*
+
+1114 All world religions
+Change three classes: Judaism (Judaism), Christianity (Christianity), Islam (Islam).
+Inherit Christianity from Judaism and Islam from Christianity.
+Requirements:
+1. In the class Solution there must be a public class Judaism (Judaism).
+2. In the class Solution there must be a public class Christianity (Christianity).
+3. The Solution class must have the public class Islam.
+4. The Christianity class must be inherited from the Judaism class.
+5. The class Islam must be inherited from the class Christianity.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public class Judaism {
+    }
+
+    public class Christianity extends Judaism {
+    }
+
+    public class Islam extends Christianity {
+    }
+}
+
+
